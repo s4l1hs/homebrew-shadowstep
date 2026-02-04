623e426f1198ef0e24389bc3477fe5a7142e792d
salihsefer36 <sefersalih017@gmail.com>
2026-02-04 23:06:52 +0300

    Update shadowstep.rb

--- Formula/shadowstep.rb
+++ Formula/shadowstep.rb
@@ -3,31 +3,30 @@ class Shadowstep < Formula
 
   desc "Advanced system artifact & metadata management suite"
   homepage "https://github.com/s4l1hs/ShadowStep"
-  url "https://files.pythonhosted.org/packages/source/s/shadowstep/shadowstep-1.0.2.tar.gz"
-  version "1.0.2"
+  url "https://files.pythonhosted.org/packages/44/11/d17844ca33e2c2fec0d41dd90e8e7fd8e3bcfc4050b9ff3d07dafb13998/shadowstep-1.0.2.tar.gz"
   sha256 "f18ab9742babb3b02546dce028c6b7c3a6f6a2a285d2928ce26acc026ecd9126"
   license "MIT"
 
-  depends_on "python@3.11"
   depends_on "libyaml"
+  depends_on "python@3.11"
 
   resource "pyyaml" do
-    url "https://files.pythonhosted.org/packages/source/p/pyyaml/PyYAML-6.0.3.tar.gz"
+    url "https://files.pythonhosted.org/packages/05/8e/961c0007c59b8dd7729d542c61a4d537767a59645b82a0b521206e1e25c2/pyyaml-6.0.3.tar.gz"
     sha256 "d76623373421df22fb4cf8817020cbb7ef15c725b9d5e45f17e189bfc384190f"
   end
 
   resource "colorama" do
-    url "https://files.pythonhosted.org/packages/source/c/colorama/colorama-0.4.6.tar.gz"
+    url "https://files.pythonhosted.org/packages/d8/53/6f443c9a4a8358a93a6792e2acffb9d9d5cb0a5cfd8802644b7b1c9a02e4/colorama-0.4.6.tar.gz"
     sha256 "08695f5cb7ed6e0531a20572697297273c47b8cae5a63ffc6d6ed5c201be6e44"
   end
 
   resource "setproctitle" do
-    url "https://files.pythonhosted.org/packages/source/s/setproctitle/setproctitle-1.3.7.tar.gz"
+    url "https://files.pythonhosted.org/packages/8d/48/49393a96a2eef1ab418b17475fb92b8fcfad83d099e678751b05472e69de/setproctitle-1.3.7.tar.gz"
     sha256 "bc2bc917691c1537d5b9bca1468437176809c7e11e5694ca79a9ca12345dcb9e"
   end
 
   resource "psutil" do
-    url "https://files.pythonhosted.org/packages/source/p/psutil/psutil-7.2.2.tar.gz"
+    url "https://files.pythonhosted.org/packages/aa/c6/d1ddf4abb55e93cebc4f2ed8b5d6dbad109ecb8d63748dd2b20ab5e57ebe/psutil-7.2.2.tar.gz"
     sha256 "0746f5f8d406af344fd547f1c8daa5f5c33dbc293bb8d6a16d80b4bb88f59372"
   end
 
